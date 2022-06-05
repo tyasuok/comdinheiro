dcc306d1298d4933eaf8eeac80f8f58128ffd7ef
Thomas Yasuoka <thomas.yasuoka@outlook.com>
2022-06-05 17:32:27 -0300

    0.1.5

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
     name = 'comdinheiro',
     packages = ['comdinheiro'],
-    version = '0.1.4',
+    version = '0.1.5',
     license='GNU General Public License v2 (GPLv2)',
     description = "Interact with comdinheiro's API",
     author = 'Thomas Yasuoka',
     author_email = 'thomas.yasuoka@outlook.com',
     url = 'https://github.com/thomas-yasuoka/comdinheiro',
-    download_url = "https://github.com/thomas-yasuoka/comdinheiro/archive/refs/tags/v0.1.4.tar.gz",
+    download_url = "https://github.com/thomas-yasuoka/comdinheiro/archive/refs/tags/v0.1.5.tar.gz",
     keywords = ['api', 'comdinheiro'],
     install_requires=[
           'requests',
